0be196e9e33289134bc8daf8f6799e658ad92092
george <79031971990@yandex.ru>
2019-02-08 12:02:26 +0300

    update another anagram solution

--- anagram_sort.pl
+++ anagram_sort.pl
@@ -3,18 +3,17 @@
 use 5.010;
 
 sub sort_string {
-    my ($str) = @_;
-    my @arr_frm_str = split(//, $str);
-    my @sort_arr_frm_str = sort @arr_frm_str;
-    $str = "";
-    for (@sort_arr_frm_str) { $str .= $_ }
-    say $str;
+    my ($str) = @_; 
+    my $res;
+    for (sort split(//, $str)) { $res .= $_ }
+    return $res;
 }
 
 sub is_anagram {
     my ($str1, $str2) = @_;
-    unless (length($str1) == length($str2)) {say "not eq len"}
-    elsif (sort_string($str1) eq sort_string($str2)) {say "eq!";}
+    unless (length($str1) == length($str2)) {say "not equal length"}
+    elsif (sort_string($str1) eq sort_string($str2)) {say "anagrams!";}
+    else {say "strigs are not anagrams"}
 }
 
 is_anagram(@ARGV);
